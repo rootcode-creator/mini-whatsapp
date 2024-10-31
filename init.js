37be3fe863152339f04101f90ff30d9e0fae346f
Kawser Ahmad <mdshakil4364@gmail.com>
2024-11-01 02:58:15 +0600

    project completed

---
+++ init.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const chat = require("./models/chat.js")
+
+main()
+  .then(() => { 
+    console.log("Connection Successful");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [ 
+    {
+    from: "neha",
+    to: "priya",
+    msg: "Send me your exam sheets",
+    created_at: new Date()
+  },
+  {
+    from: "rohit",
+    to: "mohit",
+    msg: "Teach me Js callbacks",
+    created_at: new Date()
+  },
+  {
+    from: "amit",
+    to: "sumit",
+    msg: "All the best",
+    created_at: new Date()
+  },
+
+  {
+    from: "anita",
+    to: "ramesh",
+    msg: "Bring me some fruits",
+    created_at: new Date()
+  },
+  {
+    from: "tony",
+    to: "peter",
+    msg: "Tonys message to peter",
+    created_at: new Date()
+  },
+
+];
+
+chat.insertMany(allChats);
+  
